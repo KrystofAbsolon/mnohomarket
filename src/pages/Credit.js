62e05652a80bc7a00a243f8be14758a508fad120
Kryštof Absolon <krystof.absolon@gmail.com>
2026-05-25 15:11:53 +0200

    You can now deposit whatever amount you want

--- src/pages/Credit.js
+++ src/pages/Credit.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
+
 function Credit({credit, changeCredit}) { 
 
-    function deposit10() {
-        changeCredit(10);
+    const [amount, setAmount] = useState(10);
+
+    function deposit() {
+        changeCredit(amount);
     }
 
     return (
         <div className="container">
             <h1>Chybí vám credit? Doplňte si ho!</h1>
-            <button className="btn btn-primary" onClick={deposit10}>Vložit 10$</button>
+            <input type="number" className="form-control mb-3" placeholder="Amount" value={amount} min="1" max="10000000000" onChange={(e) => {setAmount(Number(e.target.value))}} />
+            <button className="btn btn-primary" onClick={deposit}>Vložit</button>
         </div>
     )
 
