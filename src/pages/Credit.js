31c2377e8f90d102298c98921eb4145c253fa480
Kryštof Absolon <krystof.absolon@gmail.com>
2026-05-25 15:14:40 +0200

    Added withdraw button that doesnt work

--- src/pages/Credit.js
+++ src/pages/Credit.js
@@ -13,6 +13,8 @@ function Credit({credit, changeCredit}) {
             <h1>Chybí vám credit? Doplňte si ho!</h1>
             <input type="number" className="form-control mb-3" placeholder="Amount" value={amount} min="1" max="10000000000" onChange={(e) => {setAmount(Number(e.target.value))}} />
             <button className="btn btn-primary" onClick={deposit}>Vložit</button>
+            <br/>
+            <button className="btn btn-primary" onClick={window.alert("V demu nejde vybrat love")}>Vybrat</button>
         </div>
     )
 
